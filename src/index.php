35e210b8c0a437d965b2f6a696cacfd1024b543e
sammers <duncan@samwell.me>
2018-12-01 09:39:01 +0000

    Poker Timer - Initial Commit. Contains a ready to go Bootstrap 4.1.3 with SASS config. Includes all files this time

---
+++ src/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+    <head>
+        <title>NCPL - Poker Timer</title>
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:400,800">
+        <link rel='stylesheet' href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/style.min.css">
+    </head>
+
+    <body>
+        <h1>Poker Timer</h1>
+
+
+
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/app.js"></script>
+    </body>
+</html>
